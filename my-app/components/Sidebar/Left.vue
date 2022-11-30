44824b5d357a65a7b7fc17c60e84c64028e90df9
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ my-app/components/Sidebar/Left.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div
+      class="p-2 my-2 hover:bg-blue-500 rounded-full w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+
+    <div class="mt-2 space-y-3">
+      <SidebarTab>
+        <template #icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name> Home </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name> Explore </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name> Notification </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name> Messages </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <BookmarkIcon />
+        </template>
+        <template v-slot:name> Bookmarks </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <DocumentTextIcon />
+        </template>
+        <template v-slot:name> Lists </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name> Profile </template>
+      </SidebarTab>
+
+      <SidebarTab>
+        <template #icon>
+          <DotsCircleHorizontalIcon />
+        </template>
+        <template v-slot:name> More </template>
+      </SidebarTab>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+} from "@heroicons/vue/outline";
+
+const { defaultTransition } = useTailwindConfig();
+</script>
---
+++ my-app/components/Sidebar/Right.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="flex flex-col">
+    <SidebarPreviewCard title="What's happening">
+      <SidebarItem v-for="whatsHappeningItem in whatsHappeningItems">
+        <div class="flex flex-row justify-between p-2">
+          >
+          <h2 class="font-bold text-gray-800 text-md dark:text-white">
+            {{ whatsHappeningItem.title }}
+          </h2>
+          <p class="text-xs text-gray-400">{{ whatsHappeningItem.count }}</p>
+        </div>
+      </SidebarItem>
+    </SidebarPreviewCard>
+
+    <SidebarPreviewCard title="Who to follow">
+      <SidebarItem v-for="whoToFollowItem in whoToFollowItems">
+        <div class="flex flex-row justify-between items-center p-2">
+          <div class="flex flex-row">
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="whoToFollowItem.image"
+              :alt="whoToFollowItem.name"
+            />
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ whoToFollowItem.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ whoToFollowItem.handle }}</p>
+            </div>
+          </div>
+          <div class="flex h-full">
+            <button
+              class="px-4 py-2 text-xs font-bold text-white bg-black dark:text-black dark:bg-white rounded-full"
+            >
+              Follow
+            </button>
+          </div>
+        </div>
+      </SidebarItem>
+    </SidebarPreviewCard>
+  </div>
+</template>
+<script setup>
+const whatsHappeningItems = ref([
+  {
+    title: "SpaceX",
+    count: "19k Tweets",
+  },
+  {
+    title: "Clone",
+    count: "12k Tweets",
+  },
+  {
+    title: "Closure",
+    count: "19k Tweets",
+  },
+]);
+
+const whoToFollowItems = ref([
+  {
+    name: "Baiden",
+    handle: "@Biden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Baiden",
+    handle: "@Biden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Baiden",
+    handle: "@Biden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Baiden",
+    handle: "@Biden",
+    image: "https://picsum.photos/200/200",
+  },
+]);
+</script>
