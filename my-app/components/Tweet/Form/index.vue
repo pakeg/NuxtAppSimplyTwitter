d6809878f5a9f7e7ab39ca91802f5f953a5d3dc7
Pakeg Bae <pakegpakeg@gmail.com>
2023-07-05 02:02:55 +0000

    Update 15 files

---
+++ my-app/components/Tweet/Form/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div><TweetFormInput :user="props.user" @onSubmit="handleFormSubmit" /></div>
+</template>
+
+<script setup>
+const { postTweet } = useTweets();
+
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+});
+
+const handleFormSubmit = async (data) => {
+  try {
+    const response = await postTweet({
+      text: data.text,
+      mediaFiles: data.mediaFiles,
+    });
+  } catch (error) {}
+};
+</script>
--- my-app/server/user/tweets/index.post.js
+++ my-app/server/user/tweets/index.post.js
@@ -1,4 +1,8 @@
 import formidable from "formidable";
+import { createMediaFile } from "../../dp/mediaFiles";
+import { createTweet } from "../../dp/tweets";
+import { tweetTransformer } from "../../transformers/tweet";
+import { uploadToCloudinary } from "../../utils/cloudinary";
 
 export default defineEventHandler(async (event) => {
   const form = formidable({});
@@ -11,4 +15,34 @@ export default defineEventHandler(async (event) => {
       resolve({ fields, files });
     });
   });
+
+  const { fields, files } = response;
+
+  const userId = event.context?.auth?.user?.id;
+
+  const tweetData = {
+    text: fields.text,
+    authorId: userId,
+  };
+
+  const tweet = await createTweet(tweetData);
+
+  const filePromises = Object.keys(files).map(async (key) => {
+    const file = files[key];
+
+    const cloudinaryResource = await uploadToCloudinary(file.filepath);
+
+    return createMediaFile({
+      url: cloudinaryResource.secure_url,
+      providerPublicId: cloudinaryResource.public_id,
+      userId,
+      tweetId: tweet.id,
+    });
+  });
+
+  await Promise.all(filePromises);
+
+  return {
+    tweet: tweetTransformer(tweet),
+  };
 });
