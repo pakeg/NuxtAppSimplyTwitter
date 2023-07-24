907409c401221a1b016c0aa28826ec23ed3b948c
Pakeg Bae <pakegpakeg@gmail.com>
2023-07-24 19:13:20 +0000

    Update useAuth.js

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -24,7 +24,7 @@ export default () => {
   const login = ({ username, password }) => {
     return new Promise(async (resolve, reject) => {
       try {
-        const data = await fetch("/api/auth/login", {
+        const data = await $fetch("/api/auth/login", {
           method: "POST",
           body: { username: username, password: password },
         });
