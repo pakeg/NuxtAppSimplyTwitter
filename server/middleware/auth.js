643666805453c9da373e4a232c43260d47a8e514
Pakeg Bae <pakegpakeg@gmail.com>
2023-07-24 23:13:20 +0000

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -14,7 +14,7 @@ export default defineEventHandler(async (event) => {
   const isHandledByThisMiddleware = endpoints.some((endpoint) => {
     const pattern = new UrlPattern(endpoint);
 
-    return pattern.match(new UrlPattern(getRequestURL(event)));
+    return pattern.match(getRequestURL(event));
   });
 
   if (!isHandledByThisMiddleware) {
