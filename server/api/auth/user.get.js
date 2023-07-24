fd75b3dc6aea0dd13c696f9aa5b37614d4a862e9
Pakeg Bae <pakegpakeg@gmail.com>
2023-07-24 23:20:25 +0000

    Update user.get.js

--- server/api/auth/user.get.js
+++ server/api/auth/user.get.js
@@ -1,6 +1,7 @@
 import { userTransformer } from "../../transformers/users";
 
 export default defineEventHandler(async (event) => {
+  return send(event, getRequestURL(event), "json");
   return {
     user: userTransformer(event.context.auth?.user),
   };
